57281aecf057d2784d7eb861398fd18a4455e748
Yurii Halas <galasayra123213@gmail.com>
2020-01-26 19:25:32 +0200

    add some params to jenkinsfile

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,4 +1,23 @@
-node {
-    git url: 'https://github.com/yuriihalas/selenium_module_3.git'
-    echo "helloToAll"
+pipeline {
+    agent any
+    parameters {
+        string(name: 'NICK', description: 'Nickname of your stackOverflow account')
+    }
+    stages {
+        stage('Cleaning') {
+            mvn clean
+        }
+        stage('Compile') {
+            git url: 'https://github.com/yuriihalas/selenium_module_3.git'
+            echo "helloToAll"
+        }
+        stage('Tests') {
+            echo "${params.NICK}"
+        }
+        post {
+            always {
+                echo "Build is FINISHED"
+            }
+        }
+    }
 }
