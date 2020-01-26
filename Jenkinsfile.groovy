d4c8bfbe9fc8d968f69cd90e18ab85116f3fcb69
Yurii Halas <galasayra123213@gmail.com>
2020-01-26 20:17:05 +0200

    refactor jenkins file

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,23 +1,30 @@
 pipeline {
     agent any
+    initialise {
+        git url: 'https://github.com/yuriihalas/selenium_module_3.git'
+    }
     parameters {
-        string(name: 'NICK', description: 'Nickname of your stackOverflow account')
+        string(name: 'NICKNAME', description: 'Nickname of your stackOverflow account')
+        string(name: 'EMAIL', description: 'Email of your stackOverflow account')
+        string(name: 'PASSWORD', description: 'Password of your stackOverflow account')
+        string(name: 'TITLE', description: 'Set title of your stackOverflow account')
+        string(name: 'LOCATION', description: 'Set location of your stackOverflow account')
+        string(name: 'ABOUT_ME_INFO', description: 'Set about me info of your stackOverflow account')
     }
     stages {
         stage('Cleaning') {
             steps {
-                echo "Cleaning"
+                sh "mvn clean"
             }
         }
         stage('Compile') {
             steps {
-                git url: 'https://github.com/yuriihalas/selenium_module_3.git'
-                echo "helloToAll"
+                sh "mvn compile"
             }
         }
         stage('Tests') {
             steps {
-                echo "${params.NICK}"
+                sh "nvn test -Dnick=${params.NICKNAME}"
             }
         }
     }
